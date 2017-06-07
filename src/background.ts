483c26ddab40f2ad77c02d1065ca1827b2844e38
wyz <wangyizhuo@gmail.com>
2017-06-07 13:51:39 -0700

    update manifest and create backgroud.ts

---
+++ src/background.ts
@@ -0,0 +1,8 @@
+let map:Map<string, string>;
+
+function build(node: chrome.bookmarks.BookmarkTreeNode[]) {
+}
+
+function buildMap(): void {
+    chrome.bookmarks.getTree(buildMap);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        content_script: path.join(__dirname, 'src/content_script.ts'),
+        background: path.join(__dirname, 'src/background.ts'),
     },
     output: {
         path: path.join(__dirname, 'dist/js'),
